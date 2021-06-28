9d57cfee1202444a78cff4340afd7c1edbccb7d8
fkaempf <florian_kaempf@gmx.de>
2021-06-28 16:38:37 +0200

    paul fixed the data mess

--- fk_movie_align.py
+++ fk_movie_align.py
@@ -13,7 +13,7 @@ import h5py
 
 
 def askforhdf5():
-
+    global dir_path
     root = tk.Tk()
     root.title("Select hdf5 file")
 
@@ -25,17 +25,10 @@ def askforhdf5():
     return dir_path
 
 
-file = h5py.File(askforhdf5(), 'r')
-
-
-
-
-imaging_data = np.array(file['z_plane0000']['imaging_green_channel']).astype(np.uint16)
-imaging_information = np.array(file['z_plane0000']['imaging_information'])
-visual_stimulus_data = np.array(file['z_plane0000']['stimulus_information'])
-
-print(imaging_data.shape)
-
+with h5py.File(askforhdf5(), 'r') as file:
+    imaging_data = np.array(file['z_plane0000']['imaging_green_channel'])
+    imaging_information = np.array(file['z_plane0000']['imaging_information'])
+    visual_stimulus_data = np.array(file['z_plane0000']['stimulus_information'])
 
 imaging_time = imaging_information[:, 0]
 
@@ -43,19 +36,24 @@ f_imaging_interpolation_function_F = interp1d(imaging_time, imaging_data, axis=0
 
 trials_stimulus_aligned_F = dict()
 
-for stimulus_start_time, stim in zip(visual_stimulus_data["stimulus_start_times"],
-                                     visual_stimulus_data["stimulus_start_indices"]):
+stimulus_start_times = np.transpose(visual_stimulus_data)[0]
+stimulus_types = np.transpose(visual_stimulus_data)[2]
+
+
+for stimulus_start_time, stim in zip(stimulus_start_times, stimulus_types):
 
     print(stimulus_start_time, stim)
 
     ts = np.arange(stimulus_start_time - 10, stimulus_start_time + 60 + 10 - 0.25, 0.5)
-    stimulus_aligned_F = f_imaging_interpolation_function_F(ts)
-
+    stimulus_aligned_F = f_imaging_interpolation_function_F(ts).astype(np.uint16)
 
     if stim in trials_stimulus_aligned_F:
         trials_stimulus_aligned_F[stim].append(stimulus_aligned_F)
     else:
         trials_stimulus_aligned_F[stim] = [stimulus_aligned_F]
+
+del f_imaging_interpolation_function_F
+del imaging_data
 
 print('extacted stimulus data')
 
@@ -63,8 +61,8 @@ avg_response0 = np.nanmean(trials_stimulus_aligned_F[0], axis=0)
 avg_response1 = np.nanmean(trials_stimulus_aligned_F[1], axis=0)
 avg_response_diff = avg_response0 - avg_response1 + 10000
 
-imsave("z_plane0000_trial000_imaging_roi00_green_channel_motion_corrected_avg0.tif", avg_response0)
-imsave("z_plane0000_trial000_imaging_roi00_green_channel_motion_corrected_avg1.tif", avg_response1)
-imsave("z_plane0000_trial000_imaging_roi00_green_channel_motion_corrected_diff.tif", avg_response_diff)
+imsave(dir_path.split('/')[-1].split('.')[0] + "_avg0.tif", avg_response0.astype(np.uint16))
+imsave(dir_path.split('/')[-1].split('.')[1] + "_avg1.tif", avg_response1.astype(np.uint16))
+imsave(dir_path.split('/')[-1].split('.')[1] + "_diff.tif", avg_response_diff.astype(np.uint16))
 
 
